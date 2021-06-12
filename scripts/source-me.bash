413f0b15c6dbbde3750e9005aa0bcb602f915e7f
michaelmob <4530010+michaelmob@users.noreply.github.com>
2021-06-12 01:31:46 -0400

    swap scripts/ and bin/ in path

--- scripts/source-me.bash
+++ scripts/source-me.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [[ -x /etc/bash.bashrc ]] && source /etc/bash.bashrc
 
-export PATH="$PWD/bin:$PWD/scripts:$PATH"
+export PATH="$PWD/scripts:$PWD/bin:$PATH"
 for file in $PWD/bashrc.d/*.{sh,bash}; do
   source $file
 done
