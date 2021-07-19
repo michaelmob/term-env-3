a6ec28f70284451b5fd3795b34bbf026cfb2d770
michaelmob <4530010+michaelmob@users.noreply.github.com>
2021-07-19 15:55:23 -0400

    add dialog to run-me.bash

--- scripts/run-me.bash
+++ scripts/run-me.bash
@@ -2,11 +2,38 @@
 export HERE="$(realpath "${0%/*}/..")"
 export TERM=xterm
 
-"$HERE/scripts/install-tmux-appimage"
-"$HERE/scripts/install-nvim-appimage"
+ask_session_name() {
+    dialog --stdout --inputbox 'enter tmux session name (empty to select)' 10 40
+}
 
-TMUX="$HERE/bin/tmux"
-TMUXRC="$HERE/config/tmux.conf"
-CONFIG='portable-config'
+mux() {
+    "$tmux" -f "$tmuxrc" -L $config $@
+}
 
-"$TMUX" -f "$TMUXRC" -L $CONFIG new-session -A -s ${1:-default}
+main() {
+    "$HERE/scripts/install-tmux-appimage"
+    "$HERE/scripts/install-nvim-appimage"
+    
+    local tmux="$HERE/bin/tmux"
+    local tmuxrc="$HERE/config/tmux.conf"
+    local config='portable-config'
+    
+    # session name from $1 or ask for input
+    local session_name="$1"
+    if [[ -z "$session_name" ]]; then
+        session_name="$(ask_session_name)"
+    fi
+    
+    # set new_session command variable
+    # if session name is empty, set to default and open session chooser
+    local cmd="new-session -A -s ${session_name:-default}"
+    if [[ -z "$session_name" ]]; then
+        cmd+='; choose-tree -s'
+    fi
+    
+    mux $cmd
+}
+
+
+trap clear EXIT INT
+main $@
