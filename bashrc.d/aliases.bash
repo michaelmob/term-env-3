751df477b9f8d01ff6dddfdafdffccb2487d8918
michaelmob <4530010+michaelmob@users.noreply.github.com>
2021-12-25 22:13:52 -0500

    Disable gutentags for now

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -32,7 +32,7 @@ alias sv="EDITOR=$HERE/scripts/v sudoedit"
 # rc
 alias bashrc="v -p $HERE/config/bashrc $HERE/bashrc.d/"
 alias bashrcd="n $HERE/bashrc.d"
-alias nvimrc="v $HERE/config/init.nvim"
+alias nvimrc="v $HERE/config/nvim.lua"
 alias tmuxrc="v -O $HERE/config/tmux.conf"
 alias blerc="v $HERE/config/blerc"
 alias edit-aliases="v $HERE/bashrc.d/aliases.bash"
